1653b528ea6d8bb5c6c7392fbb5dc7f755271835
aditi0380 <141302751+aditi0380@users.noreply.github.com>
2023-08-18 12:21:13 +0530

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -2,51 +2,51 @@ import streamlit as st
 import pandas
 
 
-# tab1, tab2 = st.tabs(["Logo", "Menu"])
+tab1, tab2 = st.tabs(["Logo", "Menu"])
 
-# with tab1:
-#    st.header("Logo")
-#    #st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+with tab1:
+   st.header("Logo")
+   #st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
-# with tab2:
-#    st.header("Menu")
-#    #st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-#    option = st.selectbox(
-#         'How would you like to be contacted?',
-#         ('GenData', 'MedicationAdherence', 'Nps','Obesity','PriorAuthFinal','ProdIsolation','Pto','V3'))
-#    st.write('You selected:', option)
+with tab2:
+   st.header("Menu")
+   #st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+   option = st.selectbox(
+        'How would you like to be contacted?',
+        ('GenData', 'MedicationAdherence', 'Nps','Obesity','PriorAuthFinal','ProdIsolation','Pto','V3'))
+   st.write('You selected:', option)
    
 # with tab3:
 #    st.header("An owl")
 #    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
-import streamlit.components.v1 as components
-components.html(
-    """
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <ul class="nav nav-tabs">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">Dropdown</a>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="#">Action</a></li>
-      <li><a class="dropdown-item" href="#">Another action</a></li>
-      <li><a class="dropdown-item" href="#">Something else here</a></li>
-      <li><hr class="dropdown-divider"></li>
-      <li><a class="dropdown-item" href="#">Separated link</a></li>
-    </ul>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
-   """
-)
+# import streamlit.components.v1 as components
+# components.html(
+#     """
+#     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+#     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+#     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+#     <ul class="nav nav-tabs">
+#   <li class="nav-item">
+#     <a class="nav-link active" aria-current="page" href="#">Active</a>
+#   </li>
+#   <li class="nav-item dropdown">
+#     <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">Dropdown</a>
+#     <ul class="dropdown-menu">
+#       <li><a class="dropdown-item" href="#">Action</a></li>
+#       <li><a class="dropdown-item" href="#">Another action</a></li>
+#       <li><a class="dropdown-item" href="#">Something else here</a></li>
+#       <li><hr class="dropdown-divider"></li>
+#       <li><a class="dropdown-item" href="#">Separated link</a></li>
+#     </ul>
+#   </li>
+#   <li class="nav-item">
+#     <a class="nav-link" href="#">Link</a>
+#   </li>
+#   <li class="nav-item">
+#     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+#   </li>
+# </ul>
+#    """
+# )
 
