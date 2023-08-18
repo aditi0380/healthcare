2e74e25fab79ad87b1e5af144786f96b1ee4bd81
aditi0380 <141302751+aditi0380@users.noreply.github.com>
2023-08-18 11:39:12 +0530

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -10,9 +10,12 @@ with tab1:
    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
 with tab2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
+   #st.header("A dog")
+   #st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+   option = st.selectbox(
+        'How would you like to be contacted?',
+        ('Email', 'Home phone', 'Mobile phone'))
+        st.write('You selected:', option)
 with tab3:
    st.header("An owl")
    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
