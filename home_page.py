85d3e5cfc5cf77344a664d1ff2fb48dafb25c9f0
aditi0380 <141302751+aditi0380@users.noreply.github.com>
2023-08-18 11:44:38 +0530

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -3,19 +3,20 @@ import pandas
 
 import streamlit as st
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+tab1, tab2 = st.tabs(["Logo", "Menu"])
 
 with tab1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+   st.header("Logo")
+   #st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
 with tab2:
-   #st.header("A dog")
+   st.header("Menu")
    #st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
    option = st.selectbox(
         'How would you like to be contacted?',
-        ('Email', 'Home phone', 'Mobile phone'))
+        ('GenData', 'MedicationAdherence', 'Nps','Obesity','PriorAuthFinal','ProdIsolation','Pto','V3'))
    st.write('You selected:', option)
-with tab3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+   
+# with tab3:
+#    st.header("An owl")
+#    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
