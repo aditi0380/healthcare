83d46fb85cf2473d83775511882d8f27c8a31934
aditi0380 <141302751+aditi0380@users.noreply.github.com>
2023-08-18 12:54:41 +0530

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -21,8 +21,7 @@ import pandas
 #    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
 # import streamlit.components.v1 as components
-st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-')
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">')
 
 st.markdown("""
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
